603f8456c241a2432872ca3786132809ea34e843
ycm76229 <ycm76229@gmail.com>
2022-08-11 16:43:42 +0800

    update tyadmin config

--- dj_server/urls.py
+++ dj_server/urls.py
@@ -74,7 +74,7 @@ schema_view = get_schema_view(
 
 urlpatterns += [
     # url(r"^$", RedirectView.as_view(url="/swagger/")),
-    url(r"^$", RedirectView.as_view(url="/admin/")),
+    url(r"^$", RedirectView.as_view(url="/xadmin/")),
     url(
         r"^swagger(?P<format>\.json|\.yaml)$",
         schema_view.without_ui(cache_timeout=0),
