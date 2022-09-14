156b550825bf3ead1daa208d98e7e1190fead8ca
ycm76229 <ycm76229@gmail.com>
2022-09-14 16:13:58 +0800

    add button example gpcode

--- apps/news/admin.py
+++ apps/news/admin.py
@@ -28,3 +28,26 @@ class NewsAdmin(ImportExportActionModelAdmin):
     ordering = ("-created_time",)
     list_per_page = 20
     date_hierarchy = "created_time"
+
+    # # 增加自定义按钮
+    # actions = ["make_copy", "custom_button"]
+    #
+    # def custom_button(self, request, queryset):
+    #     pass
+    #
+    # # 显示的文本，与django admin一致
+    # custom_button.short_description = "测试按钮"
+    # # icon，参考element-ui icon与https://fontawesome.com
+    # custom_button.icon = "fas fa-audio-description"
+    #
+    # # 指定element-ui的按钮类型，参考https://element.eleme.cn/#/zh-CN/component/button
+    # custom_button.type = "danger"
+    #
+    # # 给按钮追加自定义的颜色
+    # custom_button.style = "color:black;"
+    #
+    # def make_copy(self, request, queryset):
+    #     pass
+    #
+    # make_copy.short_description = "复制员工"
+    # make_copy.confirm = 'are you sure?'
